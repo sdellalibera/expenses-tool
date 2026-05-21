8d05ba1a30c391ba4a682ab24eb13649cc6a3e66
sdellalibera <sdellalibera@microsoft.com>
2026-05-21 10:19:16 +0200

    restructured repo

---
+++ src/aspire/apphost.cs
@@ -0,0 +1,18 @@
+﻿#:package Aspire.Hosting.Foundry@13.3.0-preview.1.26256.5
+
+#:sdk Aspire.AppHost.Sdk@13.3.0
+
+#:project ../extraction-agent/extraction-agent.csproj
+
+using Projects;
+using Aspire.Hosting.Foundry;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var existingFoundryName = builder.AddParameter("existingFoundryName");
+var existingFoundryResourceGroup = builder.AddParameter("existingFoundryResourceGroup");
+var foundry = builder.AddFoundry("foundry").RunAsExisting(existingFoundryName,existingFoundryResourceGroup);
+
+var extraction_agent = builder.AddProject<Projects.extraction_agent>("extraction-agent");
+
+builder.Build().Run();
